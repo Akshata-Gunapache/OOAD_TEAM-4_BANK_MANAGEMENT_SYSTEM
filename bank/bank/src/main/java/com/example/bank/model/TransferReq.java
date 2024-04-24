5b6930829f1a9cb743dcd036370dbe29dbf4f535
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:22:56 +0530

    Update TransferReq.java

--- bank/bank/src/main/java/com/example/bank/model/TransferReq.java
+++ bank/bank/src/main/java/com/example/bank/model/TransferReq.java
@@ -1,1 +1,14 @@
+package com.example.bank.model;
+
+import lombok.Data;
+
+@Data
+public class TransferReq {
+    private String senderId;
+    private String ifscCode;
+    private String accNo;
+    private String amount;
+    private String transerType;
+    private String senderAccNo;
+}
 
