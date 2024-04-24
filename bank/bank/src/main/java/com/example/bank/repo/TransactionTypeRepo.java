e4faa569d721f25df3e6b9cd025feffd81521c03
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:24:03 +0530

    TransactionTypeRepo.java

--- bank/bank/src/main/java/com/example/bank/repo/TransactionTypeRepo.java
+++ bank/bank/src/main/java/com/example/bank/repo/TransactionTypeRepo.java
@@ -1,1 +1,14 @@
 
+package com.example.bank.repo;
+
+import com.example.bank.entity.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TransactionTypeRepo extends JpaRepository<TransactionType,String> {
+
+    Optional<TransactionType> findBytransferType(String transfer);
+}
