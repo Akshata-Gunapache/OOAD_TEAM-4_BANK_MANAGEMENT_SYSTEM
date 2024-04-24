6b151011d507df5d7ddad5f81ec1981a283abf1b
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:25:18 +0530

    TransferService.java

--- bank/bank/src/main/java/com/example/bank/service/TransferService.java
+++ bank/bank/src/main/java/com/example/bank/service/TransferService.java
@@ -1,1 +1,9 @@
+package com.example.bank.service;
+
+import com.example.bank.model.TransferReq;
+import org.springframework.http.ResponseEntity;
+
+public interface TransferService {
+    public ResponseEntity sendMoney(TransferReq req);
+}
 
