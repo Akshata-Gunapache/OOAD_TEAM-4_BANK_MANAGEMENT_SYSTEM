df8d90c74cf9886c8d091dbc3f9f3362bde6eb5f
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:23:31 +0530

    Update TransactionDetailsRepo.java

--- bank/bank/src/main/java/com/example/bank/repo/TransactionDetailsRepo.java
+++ bank/bank/src/main/java/com/example/bank/repo/TransactionDetailsRepo.java
@@ -1,9 +1,1 @@
-package com.example.bank.repo;
 
-import com.example.bank.entity.TransactionDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionDetailsRepo extends JpaRepository<TransactionDetails,String> {
-}
