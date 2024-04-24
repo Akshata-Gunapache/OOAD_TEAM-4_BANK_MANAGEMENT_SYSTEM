dba1006f3df160db2dc31dbf98078d3baba2ac05
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:22:30 +0530

    BankUserMapper.java

--- bank/bank/src/main/java/com/example/bank/mapper/BankUserMapper.java
+++ bank/bank/src/main/java/com/example/bank/mapper/BankUserMapper.java
@@ -1,1 +1,40 @@
 
+package com.example.bank.mapper;
+
+import com.example.bank.entity.BankUser;
+import com.example.bank.entity.UserType;
+import com.example.bank.model.BankUserReq;
+import com.example.bank.repo.UserTypeRepo;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+//Information expert
+@Component
+@Builder
+public class BankUserMapper {
+
+    @Autowired
+    private UserTypeRepo userTypeRepo;
+    public BankUser createBankUser(BankUserReq req){
+        /**
+         * Implemented the Builder pattern to map the request
+         * object to entity. This is basically used instead of creating
+         * multiple constructors for multiple optional parameters
+         *
+         */
+        return  BankUser.builder()
+                .email(req.getEmail())
+                .userType(getUserType(req.getUserType()))
+                .name(req.getName())
+                .mobile(req.getMobile())
+                .build();
+    }
+
+    private UserType getUserType(String userTypeId){
+        Optional<UserType> userTypeOptional = userTypeRepo.findById(userTypeId);
+        return userTypeOptional.isPresent() ? userTypeOptional.get() : null;
+    }
+
+}
