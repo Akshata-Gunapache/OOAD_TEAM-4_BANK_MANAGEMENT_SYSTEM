a64772356152931785002f13a20c2911e0651f73
pranavi39 <113685032+pranavi39@users.noreply.github.com>
2024-04-25 00:20:27 +0530

    UserController.java

--- bank/bank/src/main/java/com/example/bank/api/UserController.java
+++ bank/bank/src/main/java/com/example/bank/api/UserController.java
@@ -1,1 +1,26 @@
 
+package com.example.bank.api;
+
+import com.example.bank.model.BankUserReq;
+import com.example.bank.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity createUser(@RequestBody BankUserReq user){
+        return userService.createUser(user);
+    }
+
+}
